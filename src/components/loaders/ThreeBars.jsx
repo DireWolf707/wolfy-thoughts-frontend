18bd69ace531f29e9e86a893b37e4329ea2c1799
DireWolf707 <rahulsood707@gmail.com>
2023-05-01 16:08:13 +0530

    added loader

---
+++ src/components/loaders/ThreeBars.jsx
@@ -0,0 +1,13 @@
+import { Stack, LinearProgress } from "@mui/material"
+
+const ThreeBars = () => {
+  return (
+    <Stack flexGrow={1} justifyContent="center" alignItems="center" gap={1} bgcolor="#000">
+      <LinearProgress color="error" sx={{ height: "8px", width: { xs: "320px", sm: "360px" } }} />
+      <LinearProgress color="error" sx={{ height: "8px", width: { xs: "320px", sm: "360px" } }} />
+      <LinearProgress color="error" sx={{ height: "8px", width: { xs: "320px", sm: "360px" } }} />
+    </Stack>
+  )
+}
+
+export default ThreeBars
