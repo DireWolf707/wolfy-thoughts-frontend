93dca12bc51759827e726319be09c9a48d638a55
DireWolf707 <rahulsood707@gmail.com>
2023-05-03 23:26:13 +0530

    updated error event

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,21 @@
 import { io } from "socket.io-client"
+import { toast } from "react-hot-toast"
+import Toast from "../components/layouts/Toast"
 
 const socket = io(import.meta.env.VITE_SERVER_URL, {
   autoConnect: false,
   withCredentials: true,
 })
 
-socket.on("connect", () => console.log("connected"))
-socket.on("connect_error", console.log)
-socket.on("disconnect", () => console.log("disconnected"))
+let once = true
+socket.on("connect_error", (err) => {
+  console.log({ ...err })
+  if (once) {
+    toast.error(Toast("something went wrong!"))
+    once = false
+  }
+})
+socket.on("connect", () => console.log("websocket connected"))
+socket.on("disconnect", () => console.log("websocket disconnected"))
 
 export default socket
