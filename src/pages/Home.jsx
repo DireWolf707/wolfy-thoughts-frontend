46fd11aa0dae33168ac096f896ec0d6bf0499a43
DireWolf707 <rahulsood707@gmail.com>
2023-05-03 23:25:29 +0530

    clean up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,6 @@
-import socket from "../utils/socket"
 import { Stack, Typography } from "@mui/material"
-import { useEffect } from "react"
 
 const Home = () => {
-  useEffect(() => {
-    socket.connect()
-
-    return () => socket.disconnect()
-  }, [])
-
   return (
     <Stack flexGrow={1}>
       <Typography>Home</Typography>
