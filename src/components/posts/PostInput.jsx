0de9117f55c99bfac0e6a9bc8de5349759f9e8be
DireWolf707 <rahulsood707@gmail.com>
2023-05-03 23:25:00 +0530

    added create functionality

--- src/components/posts/PostInput.jsx
+++ src/components/posts/PostInput.jsx
@@ -1,15 +1,30 @@
 import { Stack, Typography, Button, Box } from "@mui/material"
 import { useState } from "react"
+import { userApi, postApi, useDispatch, dataSliceActions } from "../../store"
+import requestHandler from "../../utils/requestHandler"
 
 const PostInput = () => {
-  const [post, setPost] = useState("")
-  const exceedLimit = post.length > 200
+  const dispatch = useDispatch()
+  const [content, setContent] = useState("")
+  const exceedLimit = content.length > 200
+  const [error, setError] = useState({})
+  const [createPost, { isLoading }] = postApi.useCreatePostMutation()
+  const {
+    data: { data: user },
+  } = userApi.useFetchProfileQuery()
+
+  const createPostHandler = () =>
+    requestHandler(createPost({ data: { content } }).unwrap(), "creating post", "post created", setError).then(({ data }) => {
+      setContent("")
+      const post = { ...data, user }
+      dispatch(dataSliceActions.addPost(post))
+    })
 
   return (
     <Stack p="26px 18px 6px 18px" bgcolor="#fff" borderRadius="5px">
       <Box
-        value={post}
-        onChange={(e) => setPost(e.target.value)}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
         component="textarea"
         placeholder="Type something here..."
         height="120px"
@@ -23,12 +38,16 @@ const PostInput = () => {
         sx={{ resize: "none" }}
       />
 
+      <Typography fontSize="13px" color="red" px="6px">
+        {error.content}
+      </Typography>
+
       <Stack flexDirection="row" justifyContent="space-between" alignItems="center" p="12px 18px">
         <Typography fontWeight="bold" color={exceedLimit ? "red" : "black"}>
-          {post.length}/200
+          {content.length}/200
         </Typography>
 
-        <Button variant="contained" size="small" disabled={exceedLimit}>
+        <Button onClick={createPostHandler} variant="contained" size="small" disabled={exceedLimit || isLoading}>
           Create Post
         </Button>
       </Stack>
