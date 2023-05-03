760c0b640d486dd0c4cc66d3f8b8eba2916d27b7
DireWolf707 <rahulsood707@gmail.com>
2023-05-03 23:25:19 +0530

    added fetch feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,23 +1,35 @@
+import { useState, useEffect } from "react"
 import { Stack } from "@mui/material"
 import PostInput from "../components/posts/PostInput"
 import PostFeed from "../components/posts/PostFeed"
-
-import randomString from "../utils/randomString"
-
-const feed = [
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-  { id: randomString(), title: "heheh", content: "heheh", createdAt: Date.now(), user: { avatar: "link", username: "popin" } },
-]
+import socket from "../utils/socket"
+import { postApi, dataSliceActions, useDispatch, useSelector } from "../store"
+import requestHandler from "../utils/requestHandler"
 
 const Feed = () => {
+  const dispatch = useDispatch()
+  const [isSocketConnected, setIsSocketConnected] = useState(false)
+  const { feed } = useSelector((store) => store.data)
+  const [fetchFeed, { isLoading, isFetching, isError }] = postApi.useLazyFetchFeedQuery()
+
+  useEffect(() => {
+    const setSocketConnected = () => setIsSocketConnected(true)
+    socket.on("connect", setSocketConnected)
+    socket.connect()
+
+    requestHandler(fetchFeed({}).unwrap(), "fetching posts", "posts fetched").then(({ data, cursor }) => {
+      dispatch(dataSliceActions.initFeed({ data, cursor }))
+    })
+
+    return () => socket.off("connect", setSocketConnected).disconnect()
+  }, [])
+
+  if (!isSocketConnected || isLoading || isError) return "loading..."
+
   return (
     <Stack gap="12px" m="26px auto" sx={{ width: { xs: "350px", sm: "420px", md: "460px" } }}>
       <PostInput />
-      <PostFeed feed={feed} />
+      { feed && <PostFeed feed={feed} />}
     </Stack>
   )
 }
