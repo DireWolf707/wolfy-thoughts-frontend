ac0b6d9eb07e7f18f69bc9b72ab7c5f407fa0946
DireWolf707 <rahulsood707@gmail.com>
2023-05-05 15:56:19 +0530

    chnaged folder

--- src/pages/errors/Error404.jsx
+++ src/pages/errors/Error404.jsx
@@ -4,7 +4,7 @@ import { Player } from "@lottiefiles/react-lottie-player"
 const Error404 = () => {
   return (
     <Stack flexGrow={1} justifyContent="center" alignItems="center">
-      <Player autoplay loop src="/assets/animations/404.json" style={{ height: "480px" }} />
+      <Player autoplay loop src="/assets/animations/error/404.json" style={{ height: "480px" }} />
     </Stack>
   )
 }
